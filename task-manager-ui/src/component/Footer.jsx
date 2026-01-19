7a2d648ad0dfa1d9e8bf359cd231f29d20dfb587
Guarz1a <dotranquanganh123@gmail.com>
2026-01-19 17:45:54 +0700

    Update Footer.jsx

--- task-manager-ui/src/component/Footer.jsx
+++ task-manager-ui/src/component/Footer.jsx
@@ -4,11 +4,11 @@ function Footer() {
       <div class="container footer-layout">
         <div class="footer-left">
           <p>Địa chỉ: CS2 - Học Viện Hàng không Việt Nam</p>
-          <p>Email: namanhembanhoa@gmail.com</p>
+          <p>Email: shopquanao@gmail.com</p>
         </div>
         <div class="footer-right">
           <p>Điện thoại: 0123456789</p>
-          <p>© 2025 NĂM ANH EM BÁN HOA. Bảo lưu mọi quyền.</p>
+          <p>© Shop quần áo nhóm 1. Bảo lưu mọi quyền.</p>
         </div>
       </div>
     </footer>
