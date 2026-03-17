b342fe5c2c4d5e066f715581ac9ee178e57ef0e6
Guarz1a <dotranquanganh123@gmail.com>
2026-03-17 21:16:45 +0700

    .

--- task-manager-ui/src/main.jsx
+++ task-manager-ui/src/main.jsx
@@ -1,20 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
-);
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import ProductList from './pages/User/ProductList'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <ProductList />
-//   </React.StrictMode>,
-// )
+  </React.StrictMode>
+)
