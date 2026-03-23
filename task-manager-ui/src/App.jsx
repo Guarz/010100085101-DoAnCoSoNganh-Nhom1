4fbedc0bb85a1159709433f2caf54875630fbcc1
Guarz1a <dotranquanganh123@gmail.com>
2026-03-23 23:26:31 +0700

    update App.jsx

--- task-manager-ui/src/App.jsx
+++ task-manager-ui/src/App.jsx
@@ -9,16 +9,12 @@ import Login from "./pages/Login";
 import HomePage from "./pages/User/HomePage";
 import CartPage from "./pages/CartPage";
 import UserOrders from "./pages/User/UserOrders";
-<<<<<<< HEAD
-
 // Pages ADMIN
 import AdminHome from "./pages/Admin/AdminHome";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
 import ProductManagement from "./pages/Admin/ProductManagement";
 import ProductDetail from "./pages/Admin/ProductDetail";
 import AdminOrders from "./pages/Admin/AdminOrders";
-=======
->>>>>>> 058b4e5b885689821950145c1fa458239102bd38
 
 function App() {
 
@@ -31,32 +27,26 @@ function App() {
     <Router>
 
       <Routes>
-<<<<<<< HEAD
 
         {/* ================= USER LAYOUT ================= */}
-=======
->>>>>>> 058b4e5b885689821950145c1fa458239102bd38
         <Route element={<UserLayout />}>
 
           <Route path="/" element={<HomePage />} />
-<<<<<<< HEAD
 
           <Route path="/cart" element={<CartPage />} />
 
           <Route
             path="/user/orders"
             element={user ? <UserOrders /> : <Navigate to="/login" />}
-=======
+          />
           <Route path="/home" element={<HomePage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route 
             path="/orders" 
             element={user ? <UserOrders /> : <Navigate to="/login" replace />} 
->>>>>>> 058b4e5b885689821950145c1fa458239102bd38
           />
 
         </Route>
-<<<<<<< HEAD
 
 
         {/* ================= LOGIN ================= */}
@@ -121,10 +111,8 @@ function App() {
           element={<h2 style={{ padding: 30 }}>404 - Không tìm thấy trang</h2>}
         />
 
-=======
         <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="*" element={<h2 style={{ padding: 50, textAlign: 'center' }}>404 - Không tìm thấy trang</h2>} />
->>>>>>> 058b4e5b885689821950145c1fa458239102bd38
       </Routes>
 
     </Router>
